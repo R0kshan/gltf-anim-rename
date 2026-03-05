166979c6951dd805637b1c06e12a1c8fea4be71a
Røkshan <33148219+R0kshan@users.noreply.github.com>
2026-03-05 17:26:27 +0100

    Simplified installation using the python package manager uv (#5)

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# If venv doesn't exist, create it and install requirements
-if [ ! -d "venv" ]; then
-    python -m venv venv
-    ./venv/bin/pip install -r requirements.txt
-fi
-
-# Run the python script using the venv's python directly
-./venv/bin/python gltf_anim_rename.py "$@"
